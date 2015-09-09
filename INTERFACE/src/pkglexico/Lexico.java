119b2056f957d3cc86c0223b454fcf5389a4b6a0
Louiz <luizhs396@gmail.com>
2015-09-08 22:44:18 -0300

    CHECK END FILE COMPLETED

--- INTERFACE/src/pkglexico/Lexico.java
+++ INTERFACE/src/pkglexico/Lexico.java
@@ -28,34 +28,34 @@ import javax.swing.table.DefaultTableModel;
 
 public class Lexico {
     
-     private char caracter;
+     private Character caracter;
      private Token token = new Token();
      private static char[] memory;
      private int cont = 0;
      private static Lexico read = new Lexico();
-     
-     
+     static BufferedReader in;
+     private  static FileInputStream arquivo;
      
      public static void main(String args[]) throws Exception{
          
          Scanner ler = new Scanner(System.in);
          System.out.printf("Informe o nome de arquivo texto:\n");
          String nome = ler.nextLine(); 
+         Scanner file = new Scanner(new File(nome));
+         File ww = new File(nome);
          System.out.printf("\nConteúdo do arquivo texto:\n"); 
          int j=0;
-         
+        
          String str = "";
          try { 
+             in = new BufferedReader(new FileReader(nome));  
+        char ch; 
+        
              try (FileInputStream arq = new FileInputStream(nome)) {
+                 arquivo = arq;
                  int r;
+                
                  
-                 while ((r = arq.read()) != -1) {
-                     
-                     str += (char)r;
-                     
-                     j++;
-                     // do something with the character c
-                 }
 // lê da segunda até a última linha 
              } 
     } catch (IOException e) {
@@ -68,21 +68,58 @@ public class Lexico {
          //System.out.println();
          System.out.print(""+memory[i]);
          
-     }
+    }
     
-    
+    //Resetar ponteiro do arquivo
+            
+    /*int BUFFER_SIZE = 1000;
+
+in.mark(BUFFER_SIZE);
+read.lerCaracter();  // returns the GET
+//read.lerCaracter();  // returns the Host header
+in.reset();     // rewinds the stream back to the mark
+read.lerCaracter();  // returns the GET again*/
+   
+   
+      
     
 }
 
 
-       public  void lerCaracter()throws Exception{
-         this.caracter = memory[cont];
+    public  void lerCaracter()throws Exception{
+         //this.caracter = memory[cont];
+         int r;
+                
+         if((r = in.read()) != -1) {
+                     
+         caracter = (char)r;
          cont++;
          
          System.out.println(caracter);
          
+         
+         }
+         else{
+            System.out.println("End OF File!!!!");
+         }
+         
      }  
      
+     //Metodo para verificar o final do arquivo
+    protected boolean checkEOF() throws IOException{
+       int r;
+       int BUFFER_SIZE = 1000;
+       in.mark(BUFFER_SIZE);      
+       if((r = in.read()) != -1) {
+          in.reset(); 
+          return false;
+       }
+       else{
+          in.reset(); 
+          return true;
+       }
+        
+    }
 
     
      
@@ -92,8 +129,36 @@ public class Lexico {
 
      }
      
-     protected void TrataDigito(){
+     protected void PegaToken() throws Exception{
+         
+         if(Character.isDigit(caracter)){
+             read.TrataDigito();
+         }
+         else if(Character.isLetter(caracter)){
+                read.Trata_id_and_palavra();
+             
+         } 
+         else if(caracter.equals(':')){
+             read.TrataAtribuicao(); //<-mas isso a gente faz aqui, aproveita nishida e cria os metodos que estao faltando
+         }  
+        
+     }
 
+     
+     
+     protected void TrataDigito() throws Exception{
+          //Inicio
+        String num = "" + caracter;
+        read.lerCaracter();
+        
+        while(Character.isDigit(caracter))
+        {
+            num = num + caracter;
+            read.lerCaracter();
+        }
+        
+        token.setSimbolo("snumero");
+        token.setLexema(num);
 
      }
      
@@ -104,10 +169,7 @@ public class Lexico {
      }
      
      
-     protected void PegaToken(){
-
-
-     }
+     
      
      
      
@@ -117,8 +179,49 @@ public class Lexico {
 
      }
      
-     protected void TrataOperadorRelacional(){
-
+     protected void TrataOperadorRelacional() throws Exception{
+         String op = "" + caracter;
+         
+         switch(op)
+         {
+             
+         case "<":
+             read.lerCaracter();
+             if(caracter.equals('=')){
+                 op = op + caracter;
+                 token.setSimbolo("smanorig");
+                read.lerCaracter();
+             }  
+            else
+                 token.setSimbolo("smenor");
+                 
+         case ">":
+             read.lerCaracter();
+             if( caracter.equals('=')){
+                 op = op + caracter;
+                 token.setSimbolo("smaiorig");
+                 read.lerCaracter();
+             }   
+             else
+                 token.setSimbolo("smaior");
+             
+         case "=":
+             token.setSimbolo("sig");
+             read.lerCaracter();
+             
+         case "!":
+             read.lerCaracter();
+             if(caracter.equals('=')){
+                 
+            op = op + caracter;
+            token.setSimbolo("sdif");
+            read.lerCaracter();
+             }  
+         else
+             break;
+             
+             token.setLexema(op);
+         }
 
      }
      
